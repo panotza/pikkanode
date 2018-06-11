ab75ac60c599ebff17e2e861649a30d029a01d8e
panotza <panotza_ja@hotmail.com>
2018-06-11 19:52:12 +0700

    add table.sql

---
+++ table.sql
@@ -0,0 +1,36 @@
+create table users (
+	id int not null auto_increment,
+	email varchar(255) not null,
+	password varchar(255) not null,
+	created_at timestamp not null default now(),
+    primary key (id)
+);
+create unique index email_idx on users (email);
+
+create table pictures (
+	id int not null auto_increment,
+    caption varchar(255) not null default '',
+	created_at timestamp not null default now(),
+	created_by int not null,
+    primary key (id),
+	foreign key (created_by) references users (id)
+);
+create index created_at_idx on pictures (created_at desc);
+
+create table comments (
+    id int not null auto_increment,
+    text varchar(255) not null default '',
+    created_at timestamp not null default now(),
+    created_by int not null,
+    primary key (id),
+    foreign key (created_by) references users (id)
+);
+create index created_at_idx on comments (created_at desc);
+
+create table likes (
+    user_id int not null,
+    picture_id int not null,
+    primary key (user_id, picture_id),
+    foreign key (user_id) references users (id),
+    foreign key (picture_id) references pictures(id)
+);
